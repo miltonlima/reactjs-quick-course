a29be36852f452a146d4496d25554be255805db6
Milton Lima <milton.mlima@gmail.com>
2022-11-13 19:10:11 -0300

    Example of a React component with function

--- src/components/Primeiro.jsx
+++ src/components/Primeiro.jsx
@@ -1,10 +1,11 @@
+import React from "react"
 
 function Primeiro(){
     return (
-        <div>
+        <React.Fragment>
             <h1>Primeiro Componente</h1>
             <h2>Exemplo de um componente React</h2>
-        </div>
+        </React.Fragment>
     )
 }
 
