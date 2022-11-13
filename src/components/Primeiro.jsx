edac6473abe10dc090db9abb0fe434402dbeafc8
Milton Lima <milton.mlima@gmail.com>
2022-11-13 19:30:38 -0300

    Component based on Arrow Function

--- src/components/Primeiro.jsx
+++ src/components/Primeiro.jsx
@@ -1,12 +1,7 @@
 import React from "react"
 
-function Primeiro(){
-    return (
-        <React.Fragment>
-            <h1>Primeiro Componente</h1>
-            <h2>Exemplo de um componente React</h2>
-        </React.Fragment>
-    )
-}
-
-export default Primeiro
+export default () =>
+    <>
+        <h1>Primeiro Componente (Arrow)</h1>
+        <h2>Exemplo de um componente React</h2>
+    </>
