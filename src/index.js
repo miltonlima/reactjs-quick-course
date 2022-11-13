95bf1b320489a44ee4fde2304106652349bd3ee4
Milton Lima <milton.mlima@gmail.com>
2022-11-13 18:41:59 -0300

    Create First Component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import Primeiro from './components/Primeiro'
+
 ReactDOM.render(
     <h1>Olá React!</h1>,
     document.getElementById('root')
