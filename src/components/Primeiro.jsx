3e7484ebe309ede196dbf7878c0e56e0acfbf820
Milton Lima <milton.mlima@gmail.com>
2022-11-13 18:52:38 -0300

    Example of a React component

--- src/components/Primeiro.jsx
+++ src/components/Primeiro.jsx
@@ -1,6 +1,11 @@
 
 function Primeiro(){
-    return "Primeiro"
+    return (
+        <div>
+            <h1>Primeiro Componente</h1>
+            <h2>Exemplo de um componente React</h2>
+        </div>
+    )
 }
 
 export default Primeiro
