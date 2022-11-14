9e865c536897445e4cbbd061e00acedd461dec26
Milton Lima <milton.mlima@gmail.com>
2022-11-13 22:22:48 -0300

    Pass Child Elements to Component

---
+++ src/components/ComFilhos.jsx
@@ -0,0 +1,9 @@
+import React from 'React'
+
+export default props =>
+<div>
+    <h2>Os Filhos:</h2>
+    <div>
+        {props.children}
+    </div>
+</div>
--- src/index.js
+++ src/index.js
@@ -2,16 +2,28 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Primeiro from './components/Primeiro'
-import ComParametro from './components/ComParametro'
+// import Primeiro from './components/Primeiro'
+// import ComParametro from './components/ComParametro'
+import ComFilhos from './components/ComFilhos'
+import Card from './components/layout/Card'
 
 ReactDOM.render(
     <div>
-        <Primeiro />
-        <ComParametro titulo="Esse é o título"
-            subtitulo="Esse é o subtítulo" />
-        <ComParametro titulo="Opa"
-            subtitulo="Eba" />
+        <Card titulo="Exercício X">
+            Conteudo
+        </Card>
+
+        {/* <ComFilhos>
+            <ul>
+                <li>Ana</li>
+                <li>Bia</li>
+                <li>Carlos</li>
+                <li>Daniel</li>
+            </ul>
+        </ComFilhos> */}
+        {/* <Primeiro />
+        <ComParametro titulo="Esse é o título" subtitulo="Esse é o subtítulo" />
+        <ComParametro titulo="Opa" subtitulo="Eba" /> */}
     </div>,
     document.getElementById('root')
 )
