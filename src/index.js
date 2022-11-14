f8c3c3a667f20dc08fc08229735c76807be9c3b5
Milton Lima <milton.mlima@gmail.com>
2022-11-13 23:30:20 -0300

    Show all components

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-// import Primeiro from './components/Primeiro'
+import Primeiro from './components/Primeiro'
 // import ComParametro from './components/ComParametro'
 // import ComFilhos from './components/ComFilhos'
 import Card from './components/layout/Card'
 
 ReactDOM.render(
     <div>
+        <Card titulo="Primeiro Componente">
+            <Primeiro />
+        </Card>
         <Card titulo="Exercício X">
             Conteudo
         </Card>
